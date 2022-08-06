09c4e9277310afb51d4b989e30a23137dc4b360b
Clint Banister <clint.ban@gmail.com>
2022-08-05 21:57:36 -0500

    Add icon to executable

--- GameMute/resource.h
+++ GameMute/resource.h
@@ -3,6 +3,7 @@
 // Used by GameMute.rc
 //
 #define IDC_CONTEXTMENU                 101
+#define IDI_ICON1                       102
 #define ID_OPENDEBUGCONSOLE_EXIT        40001
 #define ID_EXIT                         40002
 #define ID_CONSOLE                      40003
@@ -11,7 +12,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40004
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
